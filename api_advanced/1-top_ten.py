5c73c282cfa10085a18f05e25f97e82618358e34
igorntwari <igorntwari28@gmail.com>
2025-11-18 16:34:55 +0200

    feat: change entire file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,40 +1,44 @@
 #!/usr/bin/python3
-"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+"""Function that queries the Reddit API"""
 
 import requests
 
 
 def top_ten(subreddit):
-    """Queries the Reddit API and prints the titles of the first 10 hot posts"""
+    """Prints titles of first 10 hot posts for a given subreddit"""
     if subreddit is None or not isinstance(subreddit, str):
         print(None)
         return
 
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {"User-Agent": "python:subreddit.top.ten:v1.0 (by /u/therealigor007)"}
+    headers = {
+        "User-Agent": "python:subreddit:v1.0 (by /u/therealigor007)"
+    }
     params = {"limit": 10}
 
     try:
-        response = requests.get(url, headers=headers, params=params, 
-                                allow_redirects=False)
-        
-        # If we get a redirect, the subreddit doesn't exist
+        response = requests.get(
+            url,
+            headers=headers,
+            params=params,
+            allow_redirects=False
+        )
+
         if response.status_code == 302:
             print(None)
             return
-        
-        # If we don't get a successful response
+
         if response.status_code != 200:
             print(None)
             return
 
         data = response.json()
         posts = data.get("data", {}).get("children", [])
-        
+
         for post in posts:
             title = post.get("data", {}).get("title")
             if title:
                 print(title)
-                
+
     except Exception:
         print(None)
