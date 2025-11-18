bef974665202514d32755477a734d9e908179e38
igorntwari <igorntwari28@gmail.com>
2025-11-18 16:33:14 +0200

    feat: change entire file

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,25 +5,36 @@ import requests
 
 
 def top_ten(subreddit):
-    """Main function"""
-    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    """Queries the Reddit API and prints the titles of the first 10 hot posts"""
+    if subreddit is None or not isinstance(subreddit, str):
+        print(None)
+        return
 
-    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "python:subreddit.top.ten:v1.0 (by /u/therealigor007)"}
+    params = {"limit": 10}
+
     try:
-        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
-
-        # Check if response is a redirect (invalid subreddit)
-        if RESPONSE.status_code in [301, 302, 303, 307, 308]:
+        response = requests.get(url, headers=headers, params=params, 
+                                allow_redirects=False)
+        
+        # If we get a redirect, the subreddit doesn't exist
+        if response.status_code == 302:
+            print(None)
+            return
+        
+        # If we don't get a successful response
+        if response.status_code != 200:
             print(None)
             return
 
-        # Check if request was successful
-        if RESPONSE.status_code != 200:
-            print(None)
-            return
-
-        HOT_POSTS = RESPONSE.json().get("data").get("children")
-        for post in HOT_POSTS:
-            print(post.get('data').get('title'))
+        data = response.json()
+        posts = data.get("data", {}).get("children", [])
+        
+        for post in posts:
+            title = post.get("data", {}).get("title")
+            if title:
+                print(title)
+                
     except Exception:
         print(None)
